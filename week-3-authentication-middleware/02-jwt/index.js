8b695a0707237a0a6972dd11509af8f9244dd6cd
arpesh28 <arpeshgadekar@gmail.com>
2023-12-19 13:14:26 +0530

    Week 3 - 02-jwt Assignment Done

--- week-3-authentication-middleware/02-jwt/index.js
+++ week-3-authentication-middleware/02-jwt/index.js
@@ -1,21 +1,38 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 const jwtPassword = "secret";
+const zod = require("zod");
+
+const schema = zod.object({
+  username: zod.string().email(),
+  password: zod.string().min(6),
+});
+// const passwordSchema = zod.
 
 function signJwt(username, password) {
-
+  const validate = schema.safeParse({ username, password });
+  if (!validate.success) return null;
+  const token = jwt.sign({ username }, jwtPassword);
+  return token;
 }
 
 function verifyJwt(token) {
-
+  try {
+    const verify = jwt.verify(token, jwtPassword);
+    return true;
+  } catch (err) {
+    return false;
+  }
 }
 
 function decodeJwt(token) {
-
+  const decode = jwt.decode(token);
+  if (decode) return true;
+  else return false;
 }
 
 module.exports = {
-    signJwt,
-    verifyJwt,
-    decodeJwt,
-    jwtPassword
-}
+  signJwt,
+  verifyJwt,
+  decodeJwt,
+  jwtPassword,
+};
